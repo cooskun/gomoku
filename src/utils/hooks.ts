f28612bdfd739be40321982ffbc1c54e4307c91b
cooskun <coskun.developer@gmail.com>
2023-05-31 23:29:22 +0200

    Move auth definition into useEffect

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import { getAuth, signInAnonymously } from "firebase/auth";
 
-const auth = getAuth();
-
 export const useAuth = () => {
   const [playerId, setPlayerId] = useState<string | null>(null);
 
   useEffect(() => {
+    const auth = getAuth();
+
     signInAnonymously(auth)
       .then((response) => setPlayerId(response.user.uid))
       .catch((error) => console.log(error));
