55395c80dfafd136722f92f7d12f31e7fcb6f7c4
cooskun <coskun.developer@gmail.com>
2023-06-12 10:26:06 +0200

    Introduce Cursor component

---
+++ src/components/Cursor.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef, FC } from "react";
+
+interface Props {
+  value: string;
+}
+
+const Cursor: FC<Props> = ({ value }) => {
+  const cursorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const cursorElement = cursorRef.current;
+
+    if (!cursorElement) return;
+
+    const handleMouseMove = (e: MouseEvent) => {
+      cursorElement.style.left = e.pageX + "px";
+      cursorElement.style.top = e.pageY + "px";
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={cursorRef}
+      className="font-nunito font-extrabold text-rose-900 text-2xl fixed pointer-events-none z-50"
+    >
+      {value}
+    </div>
+  );
+};
+
+export default Cursor;
