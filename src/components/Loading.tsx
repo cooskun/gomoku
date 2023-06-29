cd3e46a1a780fdbe5a8cbcb89a8babf486797a89
cooskun <coskun.developer@gmail.com>
2023-06-29 08:01:19 +0200

    Make sure animation not triggered if scope is not available and make loading animation sensitive for reduce motion preferences

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import { useAnimate } from "framer-motion";
+import { useAnimate, useReducedMotion } from "framer-motion";
 import { ImPlus as IconX } from "react-icons/im";
 import { BsRecordCircleFill as IconO } from "react-icons/bs";
 
@@ -10,48 +10,76 @@ const CONTAINER_ROTATION = 180;
 
 const xSelector = "span:nth-child(1)";
 const oSelector = "span:nth-child(2)";
-
 const Loading = () => {
   const containerRotation = useRef(0);
   const [scope, animate] = useAnimate();
+  const isMotionReduced = useReducedMotion();
 
   useEffect(() => {
-    const animation = async () => {
-      /* 1 */
-      animate(
-        xSelector,
-        { x: [-X, 0], rotate: [-CHILD_ROTATION, 0] },
-        { delay: DELAY }
-      );
-      await animate(oSelector, { x: [X, 0] }, { delay: DELAY });
+    if (!isMotionReduced) {
+      const animation = async () => {
+        const checkSelectors = () => {
+          return (
+            document.querySelector(xSelector) &&
+            document.querySelector(oSelector)
+          );
+        };
 
-      /* 2 */
-      containerRotation.current += CONTAINER_ROTATION;
-      await animate(scope.current, { rotate: containerRotation.current });
+        if (scope?.current && checkSelectors()) {
+          // 1
+          animate(
+            xSelector,
+            { x: [-X, 0], rotate: [-CHILD_ROTATION, 0] },
+            { delay: DELAY }
+          );
+          await animate(oSelector, { x: [X, 0] }, { delay: DELAY });
 
-      /* 3 */
-      animate(xSelector, { x: -X, rotate: [0, CHILD_ROTATION] });
-      await animate(oSelector, { x: X });
+          if (scope?.current) {
+            // 2
+            containerRotation.current += CONTAINER_ROTATION;
+            await animate(scope?.current, {
+              rotate: containerRotation.current,
+            });
+          }
 
-      /* 4 */
-      animate(xSelector, { x: 0, rotate: 0 }, { delay: DELAY });
-      await animate(oSelector, { x: 0 }, { delay: DELAY });
+          if (checkSelectors()) {
+            // 3
+            animate(xSelector, {
+              x: -X,
+              rotate: [0, CHILD_ROTATION],
+            });
+            await animate(oSelector, { x: X });
+          }
 
-      /* 5 */
-      containerRotation.current += CONTAINER_ROTATION;
-      await animate(scope.current, { rotate: containerRotation.current });
+          if (checkSelectors()) {
+            // 4
+            animate(xSelector, { x: 0, rotate: 0 }, { delay: DELAY });
+            await animate(oSelector, { x: 0 }, { delay: DELAY });
+          }
 
-      /* 6 */
-      animate(xSelector, { x: -X, rotate: -CHILD_ROTATION });
-      await animate(oSelector, { x: X });
+          if (scope?.current) {
+            // 5
+            containerRotation.current += CONTAINER_ROTATION;
+            await animate(scope?.current, {
+              rotate: containerRotation.current,
+            });
+          }
+
+          if (checkSelectors()) {
+            // 6
+            animate(xSelector, { x: -X, rotate: -CHILD_ROTATION });
+            await animate(oSelector, { x: X });
+          }
+
+          animation();
+        }
+      };
 
       animation();
-    };
+    }
+  }, [scope, animate, isMotionReduced]);
 
-    animation();
-  }, [animate, scope]);
-
-  return (
+  return !isMotionReduced ? (
     <div ref={scope} className="flex text-8xl text-indigo-900">
       <span className="-mr-1">
         <IconX />
@@ -60,6 +88,8 @@ const Loading = () => {
         <IconO />
       </span>
     </div>
+  ) : (
+    <p className="text-8xl text-indigo-900">Loading...</p>
   );
 };
 
